0e21d4d90cb1c2383219bc9de9240e9f3c92aca0
bezicalboy <mhmdrcow3363@gmail.com>
2024-10-18 02:46:02 +0700

    cleaner code

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,20 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
-import { useState, useEffect, ReactNode } from 'react';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
-import { Badge } from '@/components/ui/badge';
-import { Github, Linkedin, Mail, ExternalLink, Menu, Moon, Sun } from 'lucide-react';
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import Footer from './porto/Footer';
 import Contact from './porto/section/Contact';
 import About from './porto/section/About';
 import Projects from './porto/section/Projects';
 import Header from './porto/Header';
 import Skills from './porto/section/Skills';
+import Footer from './porto/Footer';
 
 export function Portfolio() {
   return (
